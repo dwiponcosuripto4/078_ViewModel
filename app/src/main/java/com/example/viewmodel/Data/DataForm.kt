8c5e39ed776d267f8f33bd665f5c513868f1dd46
Dwiponco Suripto <d.suripto.ft21@mail.umy.ac.id>
2023-10-22 22:14:57 +0700

    20210140078

---
+++ app/src/main/java/com/example/viewmodel/Data/DataForm.kt
@@ -0,0 +1,5 @@
+package com.example.viewmodel.Data
+
+data class DataForm(
+    val sex: String = ""
+)
---
+++ app/src/main/java/com/example/viewmodel/Data/DataSource.kt
@@ -0,0 +1,10 @@
+package com.example.viewmodel.Data
+
+import com.example.viewmodel.R
+
+object DataSource {
+    val jenis = listOf(
+        R.string.laki,
+        R.string.wanita
+    )
+}
